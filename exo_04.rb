28ec373546d06b86b4872821db797add87c9cb70
ho757uff <robindebessel.pro@gmail.com>
2023-10-03 11:44:48 +0200

    Exo_04 - OK

---
+++ exo_04.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, en quelle année es-tu né?"
+print "> "
+user_birth_year = gets.chomp.to_i
+# puts user_birth_year
+puts "Tu auras donc 100 ans en #{user_birth_year + 101} ! Incroyable !"
