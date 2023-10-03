22cb5643e7840b23507ddd2bb094640054596f2a
ho757uff <robindebessel.pro@gmail.com>
2023-10-03 18:16:14 +0200

    Exo_17 - OK

---
+++ exo_17.rb
@@ -0,0 +1,23 @@
+puts "Salut BG ! :}"
+puts "Bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?!"
+
+print "> "
+number_of_stages = gets.chomp.to_i
+
+
+spaces = " "
+tags = "#"
+
+spaces_count = number_of_stages
+tags_count = 1
+
+# ---
+
+puts "Voici la pyramide :"
+
+number_of_stages.times do |index|
+  print spaces * spaces_count
+  puts tags * tags_count
+  spaces_count -= 1
+  tags_count += 2
+end
